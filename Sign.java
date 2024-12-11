20a3407ce2158223bc88177336c6ae8959c61bf6
VictorBaron1 <victorb104@nycstudents.net>
2024-12-11 14:33:22 +0000

    sign

--- Sign.java
+++ Sign.java
@@ -9,6 +9,24 @@ public class Sign
     }
     public int numberOfLines()
     {
-        return message.length()/width + 1;
+        int lines = message.length()/width;
+        if(message.length()%width == 0) return lines;
+        return lines + 1;
+    }
+    public String getLines()
+    {
+        String s = "";
+        int i = 0;
+        if (message.length() > 0)
+        {   
+        while(i < message.length())
+            {
+                s += message.substring(i, i+1);
+                i++;
+                if(i % width == 0 && i != message.length()) s+= ";";
+            }
+            return s;
+        }    
+            else return null;
     }
 }
